88bbb692b55e723b661c1e85ad525f96b15a238c
Roshan Titus <roshantitus@gmail.com>
2016-02-04 19:53:17 +0530

    Added junit tests

--- test/com/rsinc/webretail/b2c/estore/dao/UserDaoTest.java
+++ test/com/rsinc/webretail/b2c/estore/dao/UserDaoTest.java
@@ -3,10 +3,19 @@
  */
 package com.rsinc.webretail.b2c.estore.dao;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 /**
  * @author Roshan Titus
  *
  */
 public class UserDaoTest {
 
+	@Test
+	public void testDummy()
+	{
+		Assert.fail("Not implemented!");
+	}
 }
